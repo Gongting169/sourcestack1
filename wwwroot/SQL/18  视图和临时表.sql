f38cafb09c6b62ccb5135c29a4f645da52458478
Gongting169 <2541014053@qq.com>
2020-09-28 21:55:24 +0800

    修改表表达式

---
+++ wwwroot/SQL/18  视图和临时表.sql
@@ -0,0 +1,113 @@
+﻿
+--创建求助的应答表 Response(Id, Content, AuthorId, ProblemId, CreateTime)
+CREATE TABLE Response
+(
+[Id]  INT,
+[Content] NVARCHAR(MAX),
+[AuthorId] INT  CONSTRAINT  FK_Respone_AuthorId   FOREIGN KEY   REFERENCES PROBLEM(ID),
+[ProblemId] INT ,
+[CreateTime] DATETIME,
+[ReponseAuthorName] NVARCHAR(50),
+[ProblemAuthorName]  NVARCHAR(50),
+[ProblemTitle]  NVARCHAR(50)
+);
+
+INSERT Response VALUES(1,N'数据表',5,1,'2020/8/6',N'张三',N'李四',N'编程大酒');
+INSERT Response VALUES(2,N'多少岁',6,1,'2019/1/6',N'王五',N'你是',N'见不到');
+INSERT Response VALUES(3,N'等你拿',3,1,'2018/7/1',N'赵柳',N'参加',N'数据的');
+INSERT Response VALUES(4,N'的数据',1,1,'2019/3/9',N'飞哥',N'达到',N'的深V');
+INSERT Response VALUES(5,N'往往会',3,1,'2020/9/26',N'小李',N'谁说',N'是表激');
+INSERT Response VALUES(6,N'大酒店',6,1,'2019/5/3',N'小明',N'实习',N'大保健');
+INSERT Response VALUES(7,N'那几款',5,1,'2020/8/31',N'小黄',N'李天',N'东北局 ');
+
+--然后生成一个视图 VResponse(ResponseId, Content, ResponseAuthorId，ReponseAuthorName, ProblemId, ProblemAuthorName, ProblemTitle, CreateTime)，要求该视图：
+GO
+CREATE VIEW V_VResponse ([Id], Content, [AuthorId],ReponseAuthorName, [ProblemId], ProblemAuthorName, ProblemTitle, CreateTime)
+AS 
+ SELECT [Id],[AuthorId], 
+ [ProblemId], Content,CreateTime,
+ ReponseAuthorName,ProblemAuthorName
+ ,ProblemTitle  FROM  Response
+
+--能展示应答作者的用户名、应答对应求助的标题和作者用户名 （JOIN）
+GO
+SELECT * FROM PROBLEM ;
+SELECT * FROM [USER]  ;
+SELECT R.[ReponseAuthorName],P.TITLE ,P.AUTHOR ,U.USERNAME  
+FROM  PROBLEM P 
+JOIN Response R ON R.AuthorId = P.Id  
+JOIN [USER] U ON P.ProbelmUserID  = U.ID
+
+--只显示应答时间在2020年5月27日之后的数据 （JOIN）
+SELECT * FROM Response R JOIN PROBLEM P ON  R.AuthorId = P.ID  WHERE CreateTime < '2020/5/27';
+--已被加密
+GO
+ALTER VIEW V_VResponse
+WITH ENCRYPTION
+AS 
+ SELECT [Id],[AuthorId], 
+ [ProblemId], Content,CreateTime,
+ ReponseAuthorName,ProblemAuthorName
+ ,ProblemTitle  FROM  Response
+
+--保证其使用的基表结构无法更改
+GO
+ALTER VIEW V_VResponse
+WITH SCHEMABINDING
+AS 
+ SELECT [Id],[AuthorId], 
+ [ProblemId], Content,CreateTime,
+ ReponseAuthorName,ProblemAuthorName
+ ,ProblemTitle  FROM  dbo.Response 
+--演示：在VResponse中插入一条数据（注意业务逻辑正确性），却不能在视图中显示
+GO
+ALTER VIEW V_VResponse
+WITH SCHEMABINDING
+AS 
+ SELECT [Id],[AuthorId], 
+ [ProblemId], Content,CreateTime,
+ ReponseAuthorName,ProblemAuthorName
+ ,ProblemTitle  FROM  dbo.Response 
+ WHERE CreateTime > '2020/1/1'
+
+ INSERT V_VResponse ([Id], Content, [AuthorId],ReponseAuthorName, [ProblemId], ProblemAuthorName, ProblemTitle, CreateTime)
+ VALUES(8,N'那几款',6,1,'2019/8/7',N'布局的',N'登记表',N'就看出');
+
+--修改VResponse，让其能避免上述情形
+GO
+ALTER VIEW V_VResponse
+WITH SCHEMABINDING
+AS 
+ SELECT [Id],[AuthorId], 
+ [ProblemId], Content,CreateTime,
+ ReponseAuthorName,ProblemAuthorName
+ ,ProblemTitle  FROM  dbo.Response 
+ WHERE CreateTime > '2020/1/1'
+  WITH CHECK OPTION
+
+--创建视图VProblemKeyword(ProblemId, ProblemTitle, ProblemReward, KeywordAmount)，要求该视图：
+--能反映求助的标题、使用关键字数量和悬赏
+
+
+GO
+CREATE VIEW V_VProblemKeyword(ProblemId, ProblemTitle, ProblemReward, KeywordAmount)
+AS  
+SELECT *
+FROM PROBLEM P JOIN KeywordProblem KP ON KP.ProblemID =P.ID ;
+
+GO   
+
+
+--在ProblemId上有一个唯一聚集索引
+CREATE UNIQUE CLUSTERED INDEX IX_VProblemKeyword_ProblemId ON VProblemKeyword(ProblemId);
+
+--在ProblemReward上有一个非聚集索引
+CREATE UNIQUE NONCLUSTERED INDEX IX_VProblemKeyword_ProblemReward ON VProblemKeyword(ProblemReward)
+
+
+--在基表中插入/删除数据，观察VProblemKeyword是否相应的发生变化
+
+
+
+
+
--- wwwroot/SQL/18 外键和关系模型：求助列表、keywords和Problem n;n关联。.sql
+++ wwwroot/SQL/18 外键和关系模型：求助列表、keywords和Problem n;n关联。.sql
@@ -16,11 +16,14 @@ UPDATE KEYWORD SET [NAME] =N'编程开发语言' WHERE ID = 30;
 UPDATE KEYWORD SET [NAME] =N'顾问咨询' WHERE ID = 35;
 
 
-ALTER  TABLE PROBLEM
+ALTER  TABLE Keyword
 ALTER COLUMN [ID] INT  NOT  NULL;
 
---ALTER  TABLE KEYWORD
---ADD  CONSTRAINT PK_PROBLEM_ID PRIMARY KEY ([ID]);
+
+
+ALTER  TABLE KEYWORD
+
+ADD  CONSTRAINT PK_PROBLEM_ID PRIMARY KEY ([ID]);
 
 CREATE TABLE [dbo].KeywordProblem
 (
@@ -29,32 +32,33 @@ CREATE TABLE [dbo].KeywordProblem
 );
 SELECT * FROM KeywordProblem ;
 
-INSERT KeywordProblem  VALUES(1,3);
-INSERT KeywordProblem  VALUES(1,6);
-INSERT KeywordProblem  VALUES(2,3);
-INSERT KeywordProblem  VALUES(3,6);
-INSERT KeywordProblem  VALUES(5,2);
-INSERT KeywordProblem  VALUES(1,4);
-INSERT KeywordProblem  VALUES(3,7);
-INSERT KeywordProblem  VALUES(5,3);
-INSERT KeywordProblem  VALUES(5,7);
-INSERT KeywordProblem  VALUES(2,7);
-INSERT KeywordProblem  VALUES(3,1);
+INSERT KeywordProblem  VALUES(10,3);
+INSERT KeywordProblem  VALUES(15,6);
+INSERT KeywordProblem  VALUES(20,3);
+INSERT KeywordProblem  VALUES(30,6);
+INSERT KeywordProblem  VALUES(25,2);
+INSERT KeywordProblem  VALUES(15,4);
+INSERT KeywordProblem  VALUES(30,7);
+INSERT KeywordProblem  VALUES(15,3);
+INSERT KeywordProblem  VALUES(25,7);
+INSERT KeywordProblem  VALUES(20,7);
+INSERT KeywordProblem  VALUES(30,1);
 
- ALTER TABLE Keyword2Problem
+ ALTER TABLE KeywordProblem
  ALTER  COLUMN ProblemID INT NOT  NULL;
 
-  ALTER TABLE Keyword2Problem
- ALTER  COLUMN KeywordSID INT NOT  NULL;
+  ALTER TABLE KeywordProblem
+ ALTER  COLUMN KeywordID INT NOT  NULL;
 
- ALTER TABLE Keyword2Problem ADD CONSTRAINT PK_Keyword2Problem_K2P PRIMARY KEY(KeywordSID,ProblemID);  
+ ALTER TABLE KeywordProblem 
+ ADD CONSTRAINT PK_KeywordProblem_KP PRIMARY KEY(KeywordID,ProblemID);  
 
 --求助列表：新建Keyword表，和Problem形成n:n关联（含约束）。用SQL语句演示：
 --查询获得：某求助使用了多少关键字，某关键字被多少求助使用
- SELECT * FROM KeywordS,PROBLEM,Keyword2Problem  where Keyword2Problem.KeywordSID = KeywordS.ID AND Keyword2Problem.ProblemID = Problem.ID;
+ SELECT * FROM Keyword,PROBLEM,KeywordProblem  where KeywordProblem.KeywordID = Keyword.ID AND KeywordProblem.ProblemID = Problem.ID;
 
- SELECT ProblemID ,COUNT(ProblemID)  FROM  Keyword2Problem WHERE ProblemID=1 GROUP BY   ProblemID;
- SELECT KeywordSID ,COUNT(KeywordSID)  FROM  Keyword2Problem WHERE KeywordSID=4 GROUP BY   KeywordSID;
+ SELECT ProblemID ,COUNT(ProblemID)  FROM  KeywordProblem WHERE ProblemID=1 GROUP BY   ProblemID;
+ SELECT KeywordID ,COUNT(KeywordID)  FROM  KeywordProblem WHERE KeywordID=4 GROUP BY   KeywordID;
 
 --发布了一个使用了若干个关键字的求助
 --INSERT KeywordS VALUES(7,N'原理');
@@ -62,8 +66,8 @@ INSERT KeywordProblem  VALUES(3,1);
 
 INSERT PROBLEM VALUES(8,N'试试三等奖',N'弗吉凯柏',0,23,'2019/7/1',N'飞哥',1);
 
-INSERT Keyword2Problem VALUES(7,8);
-INSERT Keyword2Problem VALUES(8,8);
+INSERT KeywordProblem VALUES(7,8);
+INSERT KeywordProblem VALUES(8,8);
 
 --该求助不再使用某个关键字
   --UPDATE KeywordS SET KeyWord = N'' WHERE ID = 8 (问题： 如果有3条求助都发布了这个关键字，只删除这条的关键字，像73这样的操作，就会把3条求助的关键字都给删除了，
@@ -72,16 +76,16 @@ INSERT Keyword2Problem VALUES(8,8);
   --DELETE Keyword2Problem WHERE KeywordSID=7 AND ProblemID=8; 正确的写法
 --删除该求助
 BEGIN TRAN 
-DELETE Keyword2Problem WHERE KeywordSID=7 AND ProblemID=8;
-DELETE Keyword2Problem WHERE KeywordSID=8 AND ProblemID=8;
+DELETE KeywordProblem WHERE KeywordID=7 AND ProblemID=8;
+DELETE KeywordProblem WHERE KeywordID=8 AND ProblemID=8;
 DELETE PROBLEM  WHERE ID =8;
 --没有去执行
 rollback;
 --删除某关键字
 BEGIN TRAN
 
-DELETE  Keyword2Problem  WHERE  KeywordSID =4;
-DELETE KeywordS WHERE KeyWord = N'JAVA'; 
+DELETE  KeywordProblem  WHERE  KeywordID =4;
+DELETE Keyword WHERE KeyWord = N'JAVA'; 
 
 或
 DELETE  Keyword2Problem  WHERE  KeywordSID =4;
@@ -89,7 +93,7 @@ DELETE  Keyword2Problem  WHERE  KeywordSID =4;
 --没有去执行
 rollback;
 
-SELECT * FROM KeywordS;
+SELECT * FROM Keyword;
 SELECT * FROM Problem;
-SELECT * FROM Keyword2Problem;
+SELECT * FROM KeywordProblem;
 
