e3eb05c955aa4c323a5e64aaa5caff46e10d55ec
Gongting169 <2541014053@qq.com>
2020-10-11 21:53:18 +0800

    提交c#运算符和表达式作业，还有面试题目

--- Csharp1/Program.cs
+++ Csharp1/Program.cs
@@ -4,22 +4,48 @@ namespace Csharp1
 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-            int a = 3, b = 2;
-            Console.WriteLine(a +b);
-            Console.WriteLine(a - b);
-            Console.WriteLine(a * b);
-            Console.WriteLine(a / b);
+            //作业：运算符和表达式
+            // int a = 3, b = 2;
+            // Console.WriteLine(a +b);
+            // Console.WriteLine(a - b);
+            // Console.WriteLine(a * b);
+            // Console.WriteLine(a / b);
 
 
-           double  n = 5.4, m = 3.6;
-            Console.WriteLine(n + m);
-            Console.WriteLine(n - m);
-            Console.WriteLine(n * m);
-            Console.WriteLine(n / m);
+            //double  n = 5.4, m = 3.6;
+            // Console.WriteLine(n + m);
+            // Console.WriteLine(n - m);
+            // Console.WriteLine(n * m);
+            // Console.WriteLine(n / m);
+
+            //int a = 23, b = 7, i = 12, j = 8, c = 6;
+            //Console.WriteLine(((a +b)*i -j )/c   );
+            //float a = 23F, b = 7F;
+            //int i = 12, j = 8;
+            //double c = 6;
+            //Console.WriteLine(((a +b)*i -j)/c );
+
+            //  int i = 15;
+            //Console.WriteLine(i++);  15 
+
+            //int i = 15;
+            //i -= 5;
+            //Console.WriteLine(i);  10   
+            //Console.WriteLine(i >= 10);  true
+
+            Console.WriteLine("i值的最终结果为：" + i);
 
 
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
