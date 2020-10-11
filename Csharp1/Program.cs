b2620015876ff9f8a09ae9d0e2ff0912e9adae65
Gongting169 <2541014053@qq.com>
2020-10-11 15:52:57 +0800

    提交c#的作业

---
+++ Csharp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Csharp1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int a = 3, b = 2;
+            Console.WriteLine(a +b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+
+
+           double  n = 5.4, m = 3.6;
+            Console.WriteLine(n + m);
+            Console.WriteLine(n - m);
+            Console.WriteLine(n * m);
+            Console.WriteLine(n / m);
+
+
+        }
+    }
+}
