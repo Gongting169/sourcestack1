00c66c364881b7097c08603a4e7846ac6abd1fdd
Gongting169 <2541014053@qq.com>
2020-11-25 17:50:10 +0800

    给以前的HTML页面添加到page页面下

---
+++ RazorPage/Pages/LogOn/LogOn.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sourcestack1.Pages.LogOn
+{
+    public class LogOnModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
