c5f81a272971502fe57ed48c48a9b25f4f8e59c3
Gongting169 <2541014053@qq.com>
2020-12-03 20:00:43 +0800

    做了TagHelper的作业，改动了Article文件下的文章列表名字

---
+++ RazorPage/DateTimeTagHelper/CalendarTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sourcestack1.DateTimeTagHelper
+{
+    [HtmlTargetElement("datetime", Attributes = "pageIndex,path")]
+    public class CalendarTagHelper : TagHelper
+    {
+    //    public override void Process(TagHelperContext context, TagHelperOutput output)
+    //    {
+    //        output.TagName = "small";
+
+    //        //分别取出pageIndex和path的值
+    //        object pageIndex = context.AllAttributes["pageIndex"].Value;
+    //        output.Attributes.Remove(context.AllAttributes["pageIndex"]);
+
+    //        object path = context.AllAttributes["path"].Value;
+    //        output.Attributes.RemoveAll("path");
+
+    //        //设置a标签里href的值
+    //        output.Attributes.Add("href", $"{path}/Page-{pageIndex}");
+    //        var content = output.GetChildContentAsync().Result.GetContent();
+    //        string box = "<span class='glyphicon glyphicon-calendar'></span>";
+    //        box += content;
+    //        output.Content.AppendHtml(box);
+    //        base.Process(context, output);
+    //    }
+    }
+}
---
+++ RazorPage/DateTimeTagHelper/DateTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sourcestack1.DateTimeTagHelper
+{
+    [HtmlTargetElement("datetime", Attributes = "pageIndex,path")]
+    public class DateTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "small";
+            //分别取出pageIndex和path的值
+            object pageIndex = context.AllAttributes["pageIndex"].Value;
+            output.Attributes.Remove(context.AllAttributes["pageIndex"]);
+
+            object path = context.AllAttributes["path"].Value;
+            output.Attributes.RemoveAll("path");
+
+            //设置a标签里href的值
+            output.Attributes.Add("href", $"{path}/Page-{pageIndex}");
+            output.Content.AppendHtml(DateTime.Now.Date.ToString("yyyy年MM月dd日"));
+            base.Process(context, output);
+        }
+    }
+}
