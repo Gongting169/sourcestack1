5df03e1d6ffd650fffad1ae72a17d83ac4f0b519
Gongting169 <2541014053@qq.com>
2020-12-06 08:25:00 +0800

    移动文件，写注册页面

--- RazorPage/Entity/User.cs
+++ RazorPage/Entity/User.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace sourcestack1.Entity
 {
-    public class User:Entity
+    public class User : Entity
     {
         public IList<Article> Articles { get; set; }
+        [Required(ErrorMessage = "用户名不能为空")]
         public string Name { get; set; }
+
+        [MaxLength(20, ErrorMessage = "密码的长度不能小于4,不能大于20")]
+        [MinLength(4, ErrorMessage = "密码的长度不能小于4，不能大于20")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage ="邀请人不能为空")]
         public User InvitedBy { get; set; }
+        [MaxLength(4,ErrorMessage ="邀请码的长度只能为4")]
+        [RegularExpression("[0 - 9] * ")]
         public string InvitedCode { get; set; }
         public int BCredit { get; set; }
         public int Reward { get; set; }
--- RazorPage/Pages/Register/Home.cshtml.cs
+++ RazorPage/Pages/Register/Home.cshtml.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using sourcestack1.Entity;
+using sourcestack1.Repository;
 
 namespace sourcestack1.Pages.Register
 {
+    [BindProperties]
     public class RegisterModel : PageModel
     {
+        private static UserRepository userRepository;
+        public RegisterModel()
+        {
+            userRepository = new UserRepository();
+        }
+        public User NewUser { get; set; }
+        public string ConfirmPassword { get; set; }
         public void OnGet()
         {
+            //AnchorTagHelper
+        }
+        public void OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
         }
     }
 }
