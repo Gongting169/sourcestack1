298a5ed3aad5b965e75aa81a3c57e226e4edd708
Gongting169 <2541014053@qq.com>
2021-01-03 20:29:46 +0800

    练习actionfilter

---
+++ MVC/Filter/ModelErrorTransferFilter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Filter
+{
+    public class ModelErrorTransferFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            ModelStateDictionary modelState = ((Controller)filterContext.Controller).ModelState;
+            if (filterContext.HttpContext.Request.HttpMethod == "post")
+            {
+                if (!modelState.IsValid)
+                {
+                    //filterContext.Controller.TempData[Keys.ErrorInModel] = modelState;
+                    filterContext.Result = new RedirectResult(filterContext.HttpContext.Request.Url.PathAndQuery);
+                }
+            }
+            else if (filterContext.HttpContext.Request.HttpMethod == "get")
+            {
+                //ModelStateDictionary error = filterContext.Controller.TempData[Keys.ErrorInModel] as ModelStateDictionary;
+                //if (error != null)
+                //{
+                //    modelState.Merge(error);
+                //}
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            base.OnResultExecuting(filterContext);
+        }
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+
+
+
+    }
+}
