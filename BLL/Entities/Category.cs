d7ffbddbef24e5d62034ee5938403042330917e3
Gongting169 <2541014053@qq.com>
2021-01-04 21:03:03 +0800

    在BLL层里的entities里面添加entity

--- BLL/Entities/Category.cs
+++ BLL/Entities/Category.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repositories
+namespace Entities
 {
-    public class Class1
+    public class Category
     {
     }
 }
