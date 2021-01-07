67b37857960fa71ef5b8fe929f5626f8bf711396
Gongting169 <2541014053@qq.com>
2021-01-07 10:29:18 +0800

    改登录页面

--- MVC/Controllers/LogController.cs
+++ MVC/Controllers/LogController.cs
@@ -1,4 +1,5 @@
-﻿using MVC.Models;
+﻿using BLL.Repositories;
+using MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,32 +10,34 @@ namespace MVC.Controllers
 {
     public class LogController : Controller
     {
+        public UserRepository userRepository;
+        public LogController()
+        {
+            userRepository = new UserRepository();
+        }
         [HttpPost]
         public ActionResult On(LogOnModel logOnModel)
         {
-            HttpCookie cookie = new HttpCookie("LogOn");
-            Response.Cookies.Add(cookie);
             if (!ModelState.IsValid)
             {
                 TempData["e"] = ModelState;
                 return RedirectToAction(nameof(On));
             }
-            //if (user == null)
-            //{
-            //    ModelState.AddModelError(nameof(NewUser.Name), "用户名不存在");
-            //    return;
-            //}
-            //if (user.Password != NewUser.Password)
-            //{
-            //    ModelState.AddModelError(nameof(NewUser.Password), "输入的密码或用户名错误");
-            //}  
-
+            if (logOnModel.Name != userRepository.GetByName(logOnModel.Name).ToString())
+            {
+                ModelState.AddModelError(nameof(logOnModel.Name), "用户名不存在");
+                return View();
+            }
+            if (logOnModel.Password != userRepository.GetByPassword(logOnModel.Password).ToString())
+            {
+                ModelState.AddModelError(nameof(logOnModel.Password), "输入的密码或用户名错误");
+            }
             return View(new LogOnModel() { RememberMe = true });
         }
         [HttpGet]
-        public ActionResult On() 
+        public ActionResult On()
         {
-            if (TempData["e"] !=null)
+            if (TempData["e"] != null)
             {
                 ModelState.Merge(TempData["e"] as ModelStateDictionary);
             }
