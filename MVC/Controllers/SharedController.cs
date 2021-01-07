3ee453818365b258a4f4dbf48ddea07946373889
Gongting169 <2541014053@qq.com>
2021-01-07 10:31:04 +0800

    增加一个sharedcontroller

---
+++ MVC/Controllers/SharedController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class SharedController : Controller
+    {
+        public ActionResult _user(int? level)
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public PartialViewResult  _logOnStatus()
+        {
+            if (true)
+            {
+
+            }
+            return PartialView();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
