01eb1a1b9c048905764d6aad5ac5f21f967042de
Gongting169 <2541014053@qq.com>
2021-01-08 14:02:28 +0800

    修改名称空间SRV层 各个项目

--- MVC/Controllers/RegisterController.cs
+++ MVC/Controllers/RegisterController.cs
@@ -20,31 +20,7 @@ namespace MVC.Controllers
         [HttpPost]
         public ActionResult Home(RegisterModel registerModel)
         {
-            //if (!ModelState.IsValid)
-            //{
-            //    TempData["e"] = ModelState;
-            //    return RedirectToAction(nameof(Home));
-            //}
-            //if (registerModel.ComfirmPassword != registerModel.Password)
-            //{
-            //    ModelState.AddModelError(nameof(registerModel.ComfirmPassword), "两次输入的密码不一致");
-            //    return RedirectToAction(nameof(Home));
-            //}
-            //if (userRepository.GetByName(registerModel.Name) != null)
-            //{
-            //    ModelState.AddModelError(nameof(registerModel.Name), " 输入的用户名已重复");
-            //    return RedirectToAction(nameof(Home));
-            //}
-            //if (userRepository.GetByName(registerModel.InvitedBy) == null)
-            //{
-            //    ModelState.AddModelError(nameof(registerModel.InvitedBy), " 邀请人不存在");
-            //    return RedirectToAction(nameof(Home));
-            //}
-            //if (userRepository.GetByInvitedCode(registerModel.InvitedCode) == null)
-            //{
-            //    ModelState.AddModelError(nameof(registerModel.InvitedCode), " 邀请人的邀请码不存在");
-            //    return RedirectToAction(nameof(Home));
-            //}
+            userService.RegisterValidate(registerModel);
             //User user = new User()
             //{
             //    Name = registerModel.Name,
