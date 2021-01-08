3b3c615f0ccda5ac7b70ffafe5825272444fec41
Gongting169 <2541014053@qq.com>
2021-01-08 10:51:02 +0800

    引入接口项目层，添加对各个项目的引用

--- ProdService/ArticleService.cs
+++ ProdService/ArticleService.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using BLL.Entities;
+using BLL.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModel;
 
 namespace ProdService
 {
-    class ArticleService
+    public class ArticleService
     {
+        private UserRepository userRepository;
+        private ArticleRepository articleRepository;
+        public ArticleService()
+        {
+            SqlDbContext context = new SqlDbContext();
+            articleRepository = new ArticleRepository(context);
+            userRepository = new UserRepository(context);
+        }
+        public void Publish(ArticleNewModel articleNewModel ,int currentUserId)
+        {
+            User author = userRepository.Find(currentUserId);
+            Article article = new Article()
+            {
+                Title = articleNewModel.Title,
+                Body = articleNewModel.Body,
+                Author = author,
+                KeyWords = articleNewModel.Keywords,
+                Category = articleNewModel.Category,
+                PublishTime = DateTime.Now,
+            };
+            //还有一个用户发布文章消耗帮帮币问题：
+            articleRepository.Save(article);
+
+        }
+
+
+
+
+
+
     }
 }
--- ProdService/UserService.cs
+++ ProdService/UserService.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using BLL.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.ModelBinding;
+using ViewModel;
 
 namespace ProdService
 {
-    class UserService
+    public class UserService
     {
+        private UserRepository userRepository;
+        public UserService()
+        {
+            SqlDbContext context = new SqlDbContext();
+            userRepository = new UserRepository(context);
+        }
+        public void RegisterValidate(RegisterModel registerModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["e"] = ModelState;
+                return RedirectToAction("Home");
+            }
+            if (registerModel.ComfirmPassword != registerModel.Password)
+            {
+                ModelState.AddModelError(nameof(registerModel.ComfirmPassword), "两次输入的密码不一致");
+                return RedirectToAction("Home");
+            }
+            if (userRepository.GetByName(registerModel.Name) != null)
+            {
+                ModelState.AddModelError(nameof(registerModel.Name), " 输入的用户名已重复");
+                return RedirectToAction("Home");
+            }
+            if (userRepository.GetByName(registerModel.InvitedBy) == null)
+            {
+                ModelState.AddModelError(nameof(registerModel.InvitedBy), " 邀请人不存在");
+                return RedirectToAction("Home");
+            }
+            if (userRepository.GetByInvitedCode(registerModel.InvitedCode) == null)
+            {
+                ModelState.AddModelError(nameof(registerModel.InvitedCode), " 邀请人的邀请码不存在");
+                return RedirectToAction("Home");
+            }
+        }
+        //public void LogOnValidate(LogOnModel logOnModel)
+        //{
+        //    if (userRepository.GetByName(logOnModel.Name) == null)
+        //    {
+        //        ModelState.AddModelError(nameof(logOnModel.Name), "用户名不存在");
+        //        return View();
+        //    }
+        //    if (logOnModel.Password != userRepository.GetByPassword(logOnModel.Password).ToString())
+        //    {
+        //        ModelState.AddModelError(nameof(logOnModel.Password), "输入的密码或用户名错误");
+        //        return View();
+        //    }
+        //}
+
+
     }
 }
