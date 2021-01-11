da5cf70de518315293cedc3352544fd61a0d5825
Gongting169 <2541014053@qq.com>
2021-01-11 19:15:42 +0800

    增加两个global的类

---
+++ GLB/Global/Keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLB.Global
+{
+    public class Keys
+    {
+        public const string ErrorInModel = "ErrorInModel";
+        public const string User = "User";
+        public const string UserId = "UserId";
+        public const string Id = "Id";
+        public const string Password = "Password";
+    }
+}
