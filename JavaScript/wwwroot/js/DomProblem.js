9201e73f92ad11f4dec667d5383ab0d26e594f45
Gongting169 <2541014053@qq.com>
2021-01-30 19:57:00 +0800

    更改以前没有做好的作业，dom元素和事件绑定

--- JavaScript/wwwroot/js/DomProblem.js
+++ JavaScript/wwwroot/js/DomProblem.js
@@ -5,8 +5,8 @@
 //var length = document.getElementsByClassName("status").length, count = 0;
 //for (var i = 0; i < length; i++)
 //{
-//    var result = document.getElementsByClassName("status")[i].
-//        getElementsByTagName("span")[4].innerText;
+//    var result = parseInt(document.getElementsByClassName("status")[i].
+//        getElementsByTagName("span")[4].innerText);
 //    if (result > 1)
 //    {
 //        count++;
@@ -14,13 +14,17 @@
 //}
 //console.log(count);
 //将状态为“协助中”的求助背景改成灰黑色//就是这个状态还是整个求助为协助中
-//document.getElementsByClassName("badge badge-pill badge-warning")[0].remove();
-//var span = document.createElement('span');   
-//span.style.backgroundColor = "#333";
-//span.setAttribute("class","badge badge-pill badge-warning");
-//var text = document.createTextNode('求助中');
-//span.appendChild(text);
-//document.getElementsByClassName("hide")[0]..appendChild(span);
+//var length = document.getElementsByName("status").length;
+//for (var i = 0; i < length; i++)
+//{
+//    var result = document.getElementsByName("status")[i].getElementsByTagName("span")[0].innerText
+//    if (result === "协助中")
+//    {
+//        document.getElementsByName("status")[i].
+//            getElementsByTagName("span")[0].style.backgroundColor = "rgba(51,51,51,0.96875)";
+//        //document.getElementsByName("status")[i].parentElement.style.backgroundColor="rgba(51,51,51,0.96875)";整个求助改为灰黑色
+//    }
+//}
 //如果总结数为0，将其从DOM树中删除 删除的是0？ 还是整个包裹住的span标签
 //var count = document.getElementsByClassName("status").length;
 //for (var i = 0; i < count; i++)
@@ -29,35 +33,42 @@
 //        .lastElementChild.getElementsByTagName("span")[2].innerText;
 //    if (result == 0)
 //    {
-//        document.getElementsByClassName("status")[i]
-//            .lastElementChild.getElementsByTagName("span")[2].remove();//删除的是0的span标签和文本
 //        //document.getElementsByClassName("status")[i]
-//        //    .lastElementChild.getElementsByTagName("span")[2].innerText.remove();
+//        //    .lastElementChild.remove();//删除的是整个span标签和文本总结和0
+//        document.getElementsByClassName("status")[i]
+//            .lastElementChild.getElementsByTagName("span")[2].remove();
 //    }// else nothing
 //}
 //写一个函数getKeywordsCount() ，可以统计某个求助使用了多少关键字 一个求助对应多个关键字
-//function getKeywordsCount()
+//var problem = document.getElementsByTagName("p")[0];
+//function getKeywordsCount(problem)
 //{
-//    var keyword=[[],[],[]],count=0,length1 = document.getElementsByTagName("p").length;
-//    for (var i = 0; i < length1; i++)
+//    var keyword = [], count = 0,length = problem.getElementsByTagName("a").length;
+//    for (var i = 0; i < length; i++)
 //    {
-//        var length2 = document.getElementsByTagName("p")[i].getElementsByTagName("a").length;
-//        for (var j = 0; j < length2; j++)
+//        keyword[i] = problem.getElementsByTagName("a")[i].innerText;
+//        if (keyword[i].length > 0)
 //        {
-//            keyword[i][j] = document.getElementsByTagName("p")[i].getElementsByTagName("a")[j].innerText;
-//            if (keyword.length >0)
-//            {             
-//                count++;
-//            }//else nothing
-//        }
+//            count++;
+//        }//else nothing
 //    }
 //    console.log(keyword);
 //    console.log(count);
 //}
 //参考用户注册页面，显示错误提示：
 //密码的长度不能小于4
-//密码和确认密码必须一致 
-
+//var password = document.getElementsByName("Password")[0];
+//if (password.value.length < 4)
+//{
+//    alert("密码的长度不能小于4");
+//}
+//密码和确认密码必须一致
+//var password = document.getElementsByName("Password")[0];
+//var confirmpassword = document.getElementsByName("ConfirmPassword")[0];
+//if (password.value != confirmpassword.value)
+//{
+//    alert("密码和确认密码必须一致");
+//}
 //参考用户资料页面，控制台显示出用户的：性别 / 出生年月 / 关注（关键字）/ 自我介绍
 //性别：
 //document.getElementsByName("gender")[0].checked;
@@ -72,20 +83,13 @@
 ////自我介绍
 //document.getElementsByName("self-introduction")[0].value;
 //实现铃铛（没有学bootstrap的同学用文字代替）闪烁效果
-//document.getElementsByTagName("i")[0].remove();
-//var span = document.getElementsByClassName("navbar-text")[0].getElementsByTagName("span")[1],
-//    tag = document.createElement("i"), text = document.createTextNode('');
-//tag.setAttribute("class", "fa fa-bell fa-2x ml-3");
-//tag.style.fontSize = "1.2rem";
-//tag.setAttribute("aria-hidden", "true");
-//tag.style.color = setInterval(function ()
+//var target = document.getElementsByTagName("i")[0];
+//function swap()
+//{
+//    target.style.color = target.style.color === "darkblue" ? "lightblue" : "darkblue";
+//}
+//target.style.color = setInterval(function ()
 //{
 //    swap();
 //}, 1000);
-//tag.appendChild(text);
-//span.after(tag);
-//function swap()
-//{
-//    tag.style.color = tag.style.color == "darkblue" ? "lightblue" : "darkblue";
-//}
 
--- JavaScript/wwwroot/js/EventBind.js
+++ JavaScript/wwwroot/js/EventBind.js
@@ -2,8 +2,42 @@
 
 //作业
 //一个“前一页”按钮，点击可以跳到上一页；一个“下一页”按钮，点击可以跳到下一页；
+//document.getElementsByTagName("button").onclick = previous;
+//function previous()
+//{
+//    alert(12); 
+// history.back();  前一页
+//}
+//document.getElementsByTagName("input")[0].onclick = next;
+//function next()
+//{
+//    alert(13);
+//    history.forward();下一页
+//}
 //一个a标签，点击不会跳转到href指定页面，而是将href的值赋值给下面的一个文本框，
+//document.getElementsByTagName("a")[0].onclick = assign;
+//function assign()
+//{
+//    window.location.reload(true);
+//    this.nextElementSibling = this.href;
+//}
 //一个“转到”按钮，点击该按钮页面可跳转到文本框指定的网址页面
+//<div>
+//    <button type="submit">转到</button>
+//    <input type="text" class="form-control" />
+//</div>
+//document.getElementsByTagName("button")[0].onclick = getPage;
+//function getPage()
+//{
+//    document.getElementsByTagName("button")[0].nextElementSibling.onclick = getTargetUrl;
+//}
+//function getTargetUrl()
+//{
+//    window.open("http://17bang.ren");
+//}
+
+
+
 
 
 
