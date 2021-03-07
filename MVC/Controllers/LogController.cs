fbc77f597251eafddb5fe7471aeeda005d08e04b
Gongting169 <2541014053@qq.com>
2021-03-07 11:26:19 +0800

    配置连接数据库的web.config 优化注册和layout页面，

--- MVC/Controllers/LogController.cs
+++ MVC/Controllers/LogController.cs
@@ -1,5 +1,6 @@
 ﻿using SRV.ProdService;
 using SRV.ServiceInterface;
+using SRV.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,43 +11,44 @@ namespace MVC.Controllers
 {
     public class LogController : Controller
     {
-        // GET: Log
-            private IUserService userService;
-            public LogController()
-            {
-                userService = new UserService();
-            }
-            //[HttpPost]
-            //public ActionResult On(LogOnModel logOnModel)
+        private IUserService userService;
+
+        public LogController()
+        {
+            userService = new SRV.ProdService.UserService();
+            //userService = new SRV.MockService.MUserService();
+        }
+        [HttpPost]
+        public ActionResult On(LogOnModel logOnModel)
+        {
+            //if (!ModelState.IsValid)
             //{
-            //    if (!ModelState.IsValid)
-            //    {
-            //        TempData["e"] = ModelState;
-            //        return RedirectToAction(nameof(On));
-            //    }
-            //    if (userRepository.GetByName(logOnModel.Name) == null)
-            //    {
-            //        ModelState.AddModelError(nameof(logOnModel.Name), "用户名不存在");
-            //        return View();
-            //    }
-            //    if (logOnModel.Password != userRepository.GetByPassword(logOnModel.Password).ToString())
-            //    {
-            //        ModelState.AddModelError(nameof(logOnModel.Password), "输入的密码或用户名错误");
-            //        return View();
-            //    }
-            //    return View(new LogOnModel() { RememberMe = true });
+            //    TempData["e"] = ModelState;
+            //    return RedirectToAction(nameof(On));
             //}
-            //[HttpGet]
-            //public ActionResult On()
+            //if (userRepository.GetByName(logOnModel.Name) == null)
             //{
-            //    if (TempData["e"] != null)
-            //    {
-            //        ModelState.Merge(TempData["e"] as ModelStateDictionary);
-            //    }
+            //    ModelState.AddModelError(nameof(logOnModel.Name), "用户名不存在");
             //    return View();
             //}
+            //if (logOnModel.Password != userRepository.GetByPassword(logOnModel.Password).ToString())
+            //{
+            //    ModelState.AddModelError(nameof(logOnModel.Password), "输入的密码或用户名错误");
+            //    return View();
+            //}
+            return View(new LogOnModel() { RememberMe = true });
+        }
+        [HttpGet]
+        public ActionResult On()
+        {
+            if (TempData["e"] != null)
+            {
+                ModelState.Merge(TempData["e"] as ModelStateDictionary);
+            }
+            return View();
+        }
 
 
-     
+
     }
 }
