1dc26f8966c9c3c8ae95fb70c8c96f94afa583ae
Gongting169 <2541014053@qq.com>
2021-03-09 20:18:51 +0800

    写了生成随机的验证码

--- Captcha/Captcha.cs
+++ Captcha/Captcha.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Captcha
 {
-   public static class Captcha
+    public class Captcha2
     {
-        /// <summary>
-        /// 生成随机的字符串
-        /// </summary>
-        /// <returns>返回的是4位的随机字符串</returns>
-        public static string GetRadomString()
+        public  void create()
         {
-            const string Letter = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
             Random random = new Random();
+            const string Letter = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
             StringBuilder sb = new StringBuilder();
             int maxrand = Letter.Length - 1;
             for (int i = 0; i < 4; i++)
@@ -24,82 +23,40 @@ namespace Captcha
                 int index = random.Next(maxrand);
                 sb.Append(Letter[index]);
             }
-            return sb.ToString();
-        }
-        /// <summary>
-        /// 生成随机的背景颜色
-        /// </summary>
-        /// <returns>返回的是Color</returns>
-        public static Color GetRadomColor()
-        {
-            int Blue = 255, Green = 255, Red = 255;
-            Random random = new Random();
-            int rblue = random.Next(Blue);
-            int rgreen = random.Next(Green);
-            int rred = random.Next(Red);
-            return Color.FromArgb(rblue, rred, rgreen);
-        }
-        /// <summary>
-        /// 生成一个自定义大小的画布
-        /// </summary>
-        /// <param name="width">宽</param>
-        /// <param name="height">高</param>
-        /// <returns>返回BitMap类型</returns>
-        public static Bitmap GetRadomBitMap(int width, int height)
-        {
-            Bitmap image = new Bitmap(width, height);
-            return image;
-        }
-       /// <summary>
-       /// 在验证码里画随机生成的直线
-       /// </summary>
-        public static void GetRadomLine()
-        {
-            Graphics graphics = Graphics.FromImage(GetRadomBitMap(500, 200));
-            Random random = new Random();
-            for (int i = 0; i < random.Next(1, 20); i++)
-            {
-                Pen linepen = new Pen(new SolidBrush(Color.Black), 3);
-                Point startpoint = new Point(random.Next(0, 500), random.Next(0, 200));
-                Point endpoint = new Point(random.Next(0, 500), random.Next(0, 200));
-                graphics.DrawLine(linepen, startpoint, endpoint);
+            string validateCode = sb.ToString();
+            Bitmap image = new Bitmap((int)Math.Ceiling(validateCode.Length * 12.0), 22);
+            Graphics g = Graphics.FromImage(image);
+                //清空图片背景色
+                g.Clear(Color.White);
+                //画图片的干扰线
+                for (int i = 0; i < 25; i++)
+                {
+                    int x1 = random.Next(image.Width);
+                    int x2 = random.Next(image.Width);
+                    int y1 = random.Next(image.Height);
+                    int y2 = random.Next(image.Height);
+                    g.DrawLine(new Pen(Color.Silver), x1, y1, x2, y2);
+                }
+                Font font = new Font("Arial", 12, (FontStyle.Bold | FontStyle.Italic));
+                LinearGradientBrush brush = new LinearGradientBrush(new Rectangle(0, 0, image.Width, image.Height),
+                 Color.Blue, Color.DarkRed, 1.2f, true);
+                g.DrawString(validateCode, font, brush, 3, 2);
+                //画图片的前景干扰点
+                for (int i = 0; i < 100; i++)
+                {
+                    int x = random.Next(image.Width);
+                    int y = random.Next(image.Height);
+                    image.SetPixel(x, y, Color.FromArgb(random.Next()));
+                }
+                //画图片的边框线
+                g.DrawRectangle(new Pen(Color.Silver), 0, 0, image.Width - 1, image.Height - 1);
+                //保存图片数据
+                image.Save(@"D:\YUANZHAN作业\Photo.jpg", ImageFormat.Jpeg);
 
-                //Point bezierPoint1 = new Point(rand.Next(0, 500), rand.Next(0, 200));
-                //Point bezierPoint2 = new Point(rand.Next(0, 500), rand.Next(0, 200));
-
-                //graph.DrawBezier(linePen, startPoint, bezierPoint1, bezierPoint2, endPoint);
-            }
         }
-        /// <summary>
-        /// 给随机的点换颜色
-        /// </summary>
-        /// <returns></returns>
-        public static Color GetRandomDeepColor()
-        {
-            Random random = new Random();
-            int red = 160, green = 100, blue = 160;
-            return Color.FromArgb(random.Next(red), random.Next(green), random.Next(blue));
-        }
-        /// <summary>
-        /// 画随机的点，以不同的颜色
-        /// </summary>
-        public static void GetRadomPoint()
-        {
-            SolidBrush fontBrush = new SolidBrush(Color.Black);
-            Font font1 = new Font("宋体", 3);
-            Random random = new Random();
-            for (int i = 0; i < 20; i++)
-            {
-                fontBrush.Color = GetRandomDeepColor();
-                float x = random.Next(0, 500);
-                float y = random.Next(0, 200);
-                Graphics.FromImage(GetRadomBitMap(500, 200)).DrawString(GetRadomString().ToString(), font1, fontBrush, x, y);
-            }
-        }
-
-
     }
-}
+}    
+
 
 
 
--- Captcha/Program.cs
+++ Captcha/Program.cs
@@ -12,57 +12,13 @@ namespace Captcha
     {
         static void Main(string[] args)
         {
-            //参考一起帮的登录页面，绘制一个验证码图片，存放到当前项目中。验证码应包含： 
-            //随机字符串
-            //混淆用的各色像素点
-            //混淆用的直线（或曲线）
-            //const string Letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            //Random random = new Random();
-            //int maxrand = Letters.Length - 1;
-            //StringBuilder sb = new StringBuilder();
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    int index = random.Next(maxrand);
-            //    sb.Append(Letters[index]);
-            //}
-            //string captcha = sb.ToString();
-            //Bitmap image = new Bitmap(500, 200);  //生成一个画板
-            //Graphics graphics = Graphics.FromImage(image);  //在画板基础上生成一个绘图对象
-            ////Random colorradom = new Random();  
-            ////graphics.Clear();
-
-            //graphics.DrawLine(new Pen(Color.HotPink), new PointF(258F, 85F), new PointF(268F, 110F));
-            //graphics.DrawLine(new Pen(Color.DarkOrange), new PointF(350.2F, 100.2F), new PointF(265F, 98F));
-            //graphics.DrawLine(new Pen(Color.GhostWhite), new PointF(165F, 93F), new PointF(240F, 106F));
-            //graphics.DrawLine(new Pen(Color.DarkBlue), new PointF(436F, 120F), new PointF(265F, 95F));
-            //graphics.DrawLine(new Pen(Color.GreenYellow), new PointF(258F, 87F), new PointF(243F, 97F));
-            //graphics.DrawLine(new Pen(Color.Orange), new PointF(300, 95.6F), new PointF(250F, 123F));
-            //graphics.DrawLine(new Pen(Color.Blue), new PointF(350.2F, 106F), new PointF(235F, 109F));
-            //graphics.DrawLine(new Pen(Color.YellowGreen), new PointF(300, 120F), new PointF(245F, 97F));
-            //graphics.DrawLine(new Pen(Color.DarkRed), new PointF(400, 105F), new PointF(260F, 115F));
-
-            //graphics.DrawString(captcha, new Font("宋体", 24), new SolidBrush(Color.DarkBlue), new PointF(225F, 90F));
-            //image.SetPixel(240, 95, Color.Yellow);
-            //image.SetPixel(245, 105, Color.GreenYellow);
-            //image.SetPixel(230, 115, Color.Red);
-            //image.SetPixel(260, 99, Color.Brown);
-            //image.SetPixel(270, 120, Color.OrangeRed);
-            //image.SetPixel(265, 110, Color.Green);
+            Captcha2 captcha2 = new Captcha2();
+            captcha2.create();
 
 
 
-
-            Captcha.GetRadomString();
-            Captcha.GetRadomBitMap(500, 200);
-            Captcha.GetRadomColor();
-            Captcha.GetRandomDeepColor();
-            Captcha.GetRadomLine();
-            Captcha.GetRadomPoint();
-            Captcha.GetRadomBitMap(500, 200).Save(@"D:\YUANZHAN作业\Photo.jpg", ImageFormat.Jpeg);
         }
-        //public static int  Red { get; }
-        //public static int  Blue { get; }
-        //public static int  Green { get; }
+
 
     }
 }
