1d39668d05d8bab678dede456c99e300bb90d4a1
Gongting169 <2541014053@qq.com>
2021-03-19 08:35:37 +0800

    增加keyword等factory，并增加model

---
+++ Tool/DbFactory/AppraiseFactory.cs
@@ -0,0 +1,28 @@
+﻿using BLL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFactory
+{
+    public class AppraiseFactory
+    {
+
+        private static void Create()
+        {
+
+        }
+        private static Appraise  Generate(string name)
+        {
+            Appraise appraise = new Appraise();
+
+            appraise.Voter = UserFactory.LiSi;
+            appraise.Article = new Article() { }
+        }
+
+
+
+    }
+}
---
+++ Tool/DbFactory/CommentFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFactory
+{
+    public class CommentFactory
+    {
+        internal  static  void Create()
+        {
+
+        }
+        private static Comment Generate() 
+        {
+            Comment comment = new Comment();
+            
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
