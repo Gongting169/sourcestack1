a21cc3ab46c59d6e5f8291fcef61d33697f50a22
Gongting169 <2541014053@qq.com>
2021-03-29 15:49:52 +0800

    增加评论的接口层和评论service，并添加方法

---
+++ SRV/ProdService/CommentService.cs
@@ -0,0 +1,21 @@
+﻿using SRV.ServiceInterface;
+using SRV.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRV.ProdService
+{
+    public class CommentService : BaseService, ICommentService
+    {
+
+
+
+       public  ArticleSingleModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
